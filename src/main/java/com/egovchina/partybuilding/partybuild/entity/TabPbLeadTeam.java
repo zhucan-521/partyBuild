ea39d441fd9d6c3ac43c9d6218a6b980aad415f1
guanyingxin <282013900@qq.com>
2019-05-06 18:58:25 +0800

    #APB-182 对党员标记，党员党籍以及领导班子及成员实体类添加注释 work 15m

--- src/main/java/com/egovchina/partybuilding/partybuild/entity/TabPbLeadTeam.java
+++ src/main/java/com/egovchina/partybuilding/partybuild/entity/TabPbLeadTeam.java
@@ -7,66 +7,159 @@ import java.util.Date;
 @Data
 public class TabPbLeadTeam {
 
+    /**
+	 * 领导班子id
+	 */
     private Long leadTeamId;
 
+    /**
+	 * 党组织主键
+	 */
     private Long orgId;
 
+    /**
+	 * 届数
+	 */
     private Long sessionYear;
 
+    /**
+	 * 换届时间", example = "yyyy-MM-dd
+	 */
     private Date changeDate;
 
+    /**
+	 * 任期年限
+	 */
     private Long duringYear;
 
+    /**
+	 * 选举方式 dict XJFS
+	 */
     private Long voteType;
 
+    /**
+	 * 应到人数
+	 */
     private Long dueCount;
 
+    /**
+	 * 实到人数
+	 */
     private Long factCount;
 
+    /**
+	 * 总票数
+	 */
     private Long voteCount;
 
+    /**
+	 * 有效票数
+	 */
     private Long validVoteCount;
 
+    /**
+	 * 当届标识
+	 */
     private Byte current;
 
+    /**
+	 * 当选方式
+	 */
     private Long electedType;
 
+    /**
+	 * 当选时间", example = "yyyy-MM-dd
+	 */
     private Date electedTime;
 
+    /**
+	 * 委员人数
+	 */
     private Long commMemNum;
 
+    /**
+	 * 委员兼职人数
+	 */
     private Long commNumParttime;
 
+    /**
+	 * 兼职委员人数
+	 */
     private Long parttimeCommNum;
 
+    /**
+	 * 常务委员人数
+	 */
     private Long standingMemNum;
 
+    /**
+	 * 候补委员人数
+	 */
     private Long backMemNum;
 
+    /**
+	 * 数据描述
+	 */
     private String description;
 
+    /**
+	 * 选举情况
+	 */
     private String voteStatus;
 
+    /**
+	 * 选举结果
+	 */
     private String voteResult;
 
+    /**
+     * 有效标记
+     */
     private String eblFlag;
 
+    /**
+     * 删除标记
+     */
     private String delFlag;
 
+    /**
+     * 排序码
+     */
     private Long orderNum;
 
+    /**
+     * 创建时间
+     */
     private Date createTime;
 
+    /**
+     * 创建用户Id
+     */
     private Long createUserid;
 
+    /**
+     * 创建人姓名
+     */
     private String createUsername;
 
+    /**
+     * 修改时间
+     */
     private Date updateTime;
 
+    /**
+     * 修改用户Id
+     */
     private Long updateUserid;
 
+    /**
+     * 修改用户姓名
+     */
     private String updateUsername;
 
+    /**
+     * 版本
+     */
     private Long version;
 
 }
--- src/main/java/com/egovchina/partybuilding/partybuild/entity/TabPbLeadTeamMember.java
+++ src/main/java/com/egovchina/partybuilding/partybuild/entity/TabPbLeadTeamMember.java
@@ -1,65 +1,159 @@
 package com.egovchina.partybuilding.partybuild.entity;
 
+import com.egovchina.partybuilding.common.entity.TabPbAttachment;
+import io.swagger.annotations.ApiModel;
 import lombok.Data;
 
 import java.util.Date;
+import java.util.List;
 
 
 @Data
 public class TabPbLeadTeamMember {
 
+    /**
+     * 班子成员id
+     */
     private Long memberId;
 
+    /**
+     * 党组织主键
+     */
     private Long orgId;
 
+    /**
+     * 领导班子id
+     */
     private Long leadTeamId;
 
+    /**
+     * 职务级别 dict JB
+     */
     private String rank;
 
+    /**
+     * 人员Id
+     */
     private Long userId;
 
+    /**
+     * 人员姓名
+     */
     private String personName;
 
+    /**
+     * 党内职务主键
+     */
     private Long positiveId;
 
+    /**
+     * 党内职务名称
+     */
     private String positiveName;
 
+    /**
+     * 任职方式
+     */
     private Long tenureMode;
 
+    /**
+     * 任职时间 yyyy-MM-dd", example = "yyyy-MM-dd
+     */
     private Date tenureBegin;
 
+    /**
+     * 任职年限
+     */
     private Long tenureDuration;
 
+    /**
+     * 离任时间 yyyy-MM-dd", example = "yyyy-MM-dd
+     */
     private Date tenureLeave;
 
+    /**
+     * 批准文号
+     */
     private String approvalNumber;
 
+    /**
+     * 是否兼任村委会委员
+     */
     private Byte asCommitteeMember;
 
+    /**
+     * 是否兼任村委主任
+     */
     private Byte asCommitteeDirector;
 
+    /**
+     * 班子职务排序
+     */
     private Long leadMemberOrder;
 
+    /**
+     * 图片
+     */
+    private String pictures;
+
+    /**
+     * 附件集合
+     */
+    private List<TabPbAttachment> attachments;
+
+    /**
+     * 数据描述
+     */
     private String description;
 
+    /**
+     * 有效标记
+     */
     private String eblFlag;
 
+    /**
+     * 删除标记
+     */
     private String delFlag;
 
+    /**
+     * 排序码
+     */
     private Long orderNum;
 
+    /**
+     * 创建时间
+     */
     private Date createTime;
 
+    /**
+     * 创建用户Id
+     */
     private Long createUserid;
 
+    /**
+     * 创建人姓名
+     */
     private String createUsername;
 
+    /**
+     * 修改时间
+     */
     private Date updateTime;
 
+    /**
+     * 修改用户Id
+     */
     private Long updateUserid;
 
+    /**
+     * 修改用户姓名
+     */
     private String updateUsername;
 
+    /**
+     * 版本
+     */
     private Long version;
 
 }
