7938e4d8b056ef9f4899d4488a58939d77a45b91
ltg <790248402@qq.com>
2019-05-08 11:25:52 +0800

    #APB-211 解决职务提示问题,解决任职职务组织名字未显示问题 work:20m

--- src/main/java/com/egovchina/partybuilding/partybuild/dto/PositivesDTO.java
+++ src/main/java/com/egovchina/partybuilding/partybuild/dto/PositivesDTO.java
@@ -28,7 +28,6 @@ public class PositivesDTO {
     private Long positiveOrgId;
 
     @ApiModelProperty(value = "职务名称，字典：党内职务（DNZW）；行政职务（HZZW）", required = true)
-    @NotNull(message = "任职党组织名称不能为空")
     private Long positiveName;
 
     @ApiModelProperty(value = "职务级别 ZWJB")
