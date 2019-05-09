209b555bce24c8a6079d958f34900dd96c4cc9f4
asd526747 <1667752601@qq.com>
2019-05-09 20:30:25 +0800

    #APB-253 修改街道大工委条件查询接口 work 1h

---
+++ src/main/java/com/egovchina/partybuilding/partybuild/entity/StreetCommitteeMemberQueryBean.java
@@ -0,0 +1,25 @@
+package com.egovchina.partybuilding.partybuild.entity;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@ApiModel("大工委成员查询实体")
+public class StreetCommitteeMemberQueryBean {
+    @ApiModelProperty(value = "列表范围 0 查所有；1 查当前组织及其直属组织； 2 查当前组织及所有下级组织")
+    private Long orgRange;
+
+    @ApiModelProperty(value = "组织主键")
+    @NotNull(message = "组织主键不能为空")
+    private Long orgId;
+
+    @ApiModelProperty(value = "成员名称")
+    private String personName;
+
+    @ApiModelProperty(value = "职位")
+    private String positiveName;
+
+}
