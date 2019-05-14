87fe8f4972c1ca91d7a47d3fbafdd670742790d9
asd526747 <1667752601@qq.com>
2019-05-14 15:16:26 +0800

    #APB-321 组织新增`领域类别`字段及查询条件 work 30m

--- src/main/java/com/egovchina/partybuilding/partybuild/vo/OrganizationVO.java
+++ src/main/java/com/egovchina/partybuilding/partybuild/vo/OrganizationVO.java
@@ -150,4 +150,9 @@ public class OrganizationVO {
 
     @ApiModelProperty(value = "单位列表")
     private List<UnitInfoVO> units;
+
+    @ApiModelProperty("领域类别 dict LYLB 多个用,号拼接")
+    @JsonSerialize(using = DictSerializer.class)
+    private String domainCategory;
+
 }
