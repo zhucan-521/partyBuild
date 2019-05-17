dd3a381671c7b47c463c1c7c90d40004d2ea04c5
ronaldo <2281572856@qq.com>
2019-05-17 16:43:03 +0800

    #APB-412 党组织-党小组模块-代码优化 work 20m

--- src/main/java/com/egovchina/partybuilding/partybuild/repository/TabPbPartyGroupMemberMapper.java
+++ src/main/java/com/egovchina/partybuilding/partybuild/repository/TabPbPartyGroupMemberMapper.java
@@ -34,34 +34,14 @@ public interface TabPbPartyGroupMemberMapper {
     List<PartyGroupMemberVO> selectPartyGroupMembersDetailsInfoByGroupId(Long groupId);
 
     /**
-     * desc: 根据党小组ID获取该小组下所有成员基本信息
-     *
-     * @param groupId 党小组ID
-     * @return List<PartyGroupMemberVO>
-     * @author FanYanGen
-     * @date 2019/4/30 11:08
-     **/
-    List<PartyGroupMemberVO> selectPartyGroupMembersBaseInfoByGroupId(Long groupId);
-
-    /**
      * desc: 根据党小组ID批量删除成员
-     *
-     * @param tabPbPartyGroupMember 党小组成员实体
-     * @return int
-     * @author FanYanGen
-     * @date 2019/4/30 10:47
-     **/
-    int batchDeleteByGroupId(TabPbPartyGroupMember tabPbPartyGroupMember);
-
-    /**
-     * desc: 根据党员ID批量删除成员
      *
      * @param groupId 党组ID
      * @return int
      * @author FanYanGen
      * @date 2019/5/6 9:23
      **/
-    int batchDeleteByUserId(@Param(value = "groupId") Long groupId);
+    int batchDeleteByGroupId(@Param(value = "groupId") Long groupId);
 
     /**
      * desc: 批量新增成员到党小组
@@ -72,16 +52,6 @@ public interface TabPbPartyGroupMemberMapper {
      * @date 2019/5/5 15:03
      **/
     int batchInsert(List<TabPbPartyGroupMember> partyGroupMembers);
-
-    /**
-     * desc: 根据主键查询该条数据是否存在
-     *
-     * @param userId 党员ID
-     * @return int
-     * @author FanYanGen
-     * @date 2019/4/29 14:42
-     **/
-    boolean checkIsExistByUserId(Long userId);
 
     /**
      * desc: 筛选指定组织中未在任何党小组存在的党员
