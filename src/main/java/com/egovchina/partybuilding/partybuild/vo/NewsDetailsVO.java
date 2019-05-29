82dff4e36a953536c461fce6da3992415ff7550b
ronaldo <2281572856@qq.com>
2019-05-29 19:07:10 +0800

    #APB-628 组织生活--党务公开--查询详情，没返回数据 work 20m

--- src/main/java/com/egovchina/partybuilding/partybuild/vo/NewsDetailsVO.java
+++ src/main/java/com/egovchina/partybuilding/partybuild/vo/NewsDetailsVO.java
@@ -1,12 +1,10 @@
 package com.egovchina.partybuilding.partybuild.vo;
 
 import com.egovchina.partybuilding.common.entity.TabPbAttachment;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -17,52 +15,17 @@ import java.util.List;
 @ApiModel("党建资讯(详情)-视图对象")
 public class NewsDetailsVO {
 
-    @ApiModelProperty(value = "资讯id")
+    @ApiModelProperty("主键ID")
     private Long newsId;
-
-    @ApiModelProperty(value = "标题")
-    private String title;
-
-    @ApiModelProperty(value = "发布组织id")
-    private String orgId;
 
     @ApiModelProperty(value = "发布组织名称")
     private String orgName;
 
-    @ApiModelProperty(value = "发布组织简称")
-    private String orgShortName;
-
-    @ApiModelProperty(value = "发布时间")
-    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
-    private Date publishTime;
-
-    @ApiModelProperty(value = "发布状态 1发布；0未发布；默认0")
-    private Integer publishStatus;
-
-    @ApiModelProperty(value = "操作人")
-    private String operator;
-
-    @ApiModelProperty(value = "操作时间")
-    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
-    private Date operatingTime;
-
-    @ApiModelProperty(value = "操作人所属党组织名称")
-    private String operatorPartyOrganizationName;
-
-    @ApiModelProperty(value = "操作人所属党组织简称")
-    private String operatorPartyOrganizationShortName;
-
     @ApiModelProperty(value = "封面图")
     private String cover;
 
-    @ApiModelProperty(value = "浏览数")
-    private Long watchNum;
-
     @ApiModelProperty(value = "资讯内容")
     private String content;
-
-    @ApiModelProperty(value = "排序码")
-    private String orderNum;
 
     @ApiModelProperty(value = "接收方信息")
     private List<NewsReceiveVO> receives;
