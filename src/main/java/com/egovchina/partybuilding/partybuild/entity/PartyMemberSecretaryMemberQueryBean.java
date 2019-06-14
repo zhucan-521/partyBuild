275aa09510bce4f7c103d7f00e3f06d0ee59b916
guanyingxin <282013900@qq.com>
2019-06-14 11:16:10 +0800

    #APB-811 班子成员相关接口调整 work 1m

--- src/main/java/com/egovchina/partybuilding/partybuild/entity/PartyMemberSecretaryMemberQueryBean.java
+++ src/main/java/com/egovchina/partybuilding/partybuild/entity/PartyMemberSecretaryMemberQueryBean.java
@@ -15,9 +15,6 @@ public class PartyMemberSecretaryMemberQueryBean {
     @ApiModelProperty(value = "名字")
     private String realname;
 
-    @ApiModelProperty(value = "组织范围 1 当前组织（包括一级下级组织）2当前组织（包含所有下级组织） 其他值 当前组织")
-    private String orgRange;
-
     @ApiModelProperty(value = "组织ID", required = true)
     @NotNull(message = "组织id不能为空")
     private Long rangeDeptId;
