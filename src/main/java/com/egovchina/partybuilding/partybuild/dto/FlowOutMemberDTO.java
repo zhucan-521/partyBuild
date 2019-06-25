f5b731b3183c8d9a593ad0064baba856bb0642c9
zhucan <zhucan126vip@126.com>
2019-06-25 09:44:41 +0800

    #APB-933 流出党员--新增，接口没控制流入组织参数必填 朱灿 work 1h

--- src/main/java/com/egovchina/partybuilding/partybuild/dto/FlowOutMemberDTO.java
+++ src/main/java/com/egovchina/partybuilding/partybuild/dto/FlowOutMemberDTO.java
@@ -3,6 +3,7 @@ package com.egovchina.partybuilding.partybuild.dto;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotNull;
@@ -13,15 +14,18 @@ import java.util.Date;
 public class FlowOutMemberDTO {
 
     @ApiModelProperty(value = "流出党员信息ID")
+    @NotNull(message = "流出党员信息ID不能为空",groups = Update.class)
     private Long flowOutId;
 
-    @ApiModelProperty(value = "用户主键")
+    @ApiModelProperty(value = "用户主键",required = true)
+    @NotNull(message = "用户主键不能为空")
     private Long userId;
 
-    @ApiModelProperty(value = "用户姓名")
+    @ApiModelProperty(value = "用户姓名",required = true)
     private String username;
 
-    @ApiModelProperty(value = "流动证号")
+    @ApiModelProperty(value = "流动证号",required = true)
+    @NotNull(message = "流动证号不能为空")
     private String flowToOrgnizeCode;
 
     @ApiModelProperty(value = "性别")
@@ -30,19 +34,23 @@ public class FlowOutMemberDTO {
     @ApiModelProperty(value = "身份证号码")
     private String idCardNo;
 
-    @ApiModelProperty(value = "原职业 码表ZY")
+    @ApiModelProperty(value = "原职业 码表ZY",required = true)
+    @NotNull(message = "原职业不能为空")
     private Long outProfession;
 
-    @ApiModelProperty(value = "外出原因 码表LDYY")
+    @ApiModelProperty(value = "外出原因 码表LDYY",required = true)
+    @NotNull(message = "外出原因不能为空")
     private Long flowOutReason;
 
-    @ApiModelProperty(value = "流动类型 码表LDQK")
+    @ApiModelProperty(value = "流动类型 码表LDQK",required = true)
+    @NotNull(message = "流动类型不能为空")
     private Long flowOutType;
 
-    @ApiModelProperty(value = "流出范围 码表WCHY")
+    @ApiModelProperty(value = "流出范围 码表WCHY",required = true)
+    @NotNull(message = "流出范围不能为空")
     private Long outIndustry;
 
-    @ApiModelProperty(value = "流出组织名称")
+    @ApiModelProperty(value = "流出组织名称",required = true)
     @NotNull(message = "请选择流出组织名称")
     private String flowFromOrgName;
 
@@ -50,7 +58,8 @@ public class FlowOutMemberDTO {
     private Long orgId;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
-    @ApiModelProperty(value = "流出日期",example = "yyyy-MM-dd")
+    @ApiModelProperty(value = "流出日期", example = "yyyy-MM-dd")
+    @NotNull(message = "流出日期不能为空")
     private Date flowOutDate;
 
     @ApiModelProperty(value = "流出组织联系人")
@@ -59,7 +68,7 @@ public class FlowOutMemberDTO {
     @ApiModelProperty(value = "流出组织联系电话")
     private String flowFromOrgPhone;
 
-    @ApiModelProperty(value = "流入组织名称")
+    @ApiModelProperty(value = "流入组织名称",required = true)
     @NotNull(message = "请选择流入组织名称")
     private String flowToOrgName;
 
@@ -76,7 +85,7 @@ public class FlowOutMemberDTO {
     private String flowToOrgContactor;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
-    @ApiModelProperty(value = "失联时间",example = "yyyy-MM-dd")
+    @ApiModelProperty(value = "失联时间", example = "yyyy-MM-dd")
     private Date lostTime;
 
     @ApiModelProperty(value = "联系情况  码表LXQK")
@@ -85,7 +94,7 @@ public class FlowOutMemberDTO {
     @ApiModelProperty(value = "状态 码表值LDGC")
     private Long flowOutState;
 
-    @ApiModelProperty(value = "流入时间",example = "yyyy-MM-dd")
+    @ApiModelProperty(value = "流入时间", example = "yyyy-MM-dd")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date flowInDate;
 
